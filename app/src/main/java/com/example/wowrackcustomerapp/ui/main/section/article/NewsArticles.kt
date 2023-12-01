2f3777d087539318c47014146b703c367ca1cce1
KevinZakky <kevinzakky9@gmail.com>
2023-12-01 21:30:03 +0700

    update skala ukuran

---
+++ app/src/main/java/com/example/wowrackcustomerapp/ui/main/section/article/NewsArticles.kt
@@ -0,0 +1,12 @@
+package com.example.wowrackcustomerapp.ui.main.section.article
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wowrackcustomerapp.R
+
+class NewsArticles : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_articles)
+    }
+}
